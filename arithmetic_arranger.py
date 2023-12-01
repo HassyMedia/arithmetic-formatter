780f0ac65b4a87764c782a3ca48ad99b22630024
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-01 17:04:47 -0500

    Added problem format validation and implemented operator and digit validation.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -7,3 +7,18 @@ def arithmetic_arranger(problems, display_answers=False):
     second_line = ""
     dashes_line = ""
     answers_line = ""
+
+    for index, problem in enumerate(problems):
+        # Splitting each problem into parts
+        parts = problem.split()
+        # Error handling for invalid problem format
+        if len(parts) != 3:
+            return "Error: Invalid problem format."
+
+        first_number, operator, second_number = parts
+
+        # Ensuring correct operator and digit-only operands
+        if operator not in ['+', '-']:
+            return "Error: Operator must be '+' or '-'."
+        if not (first_number.isdigit() and second_number.isdigit()):
+            return "Error: Numbers must only contain digits."
